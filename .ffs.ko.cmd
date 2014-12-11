1739695a4041008f0fefbbb5992797096665ba9b
alexsternberg <alexbsternberg@gmail.com>
2014-12-11 14:51:05 -0800

    Delete .ffs.ko.cmd

--- .ffs.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/alex.sternberg/projects/forwardfs.git/ffs.ko := ld -r -m elf_i386 -T /usr/src/kernels/3.11.10-301.fc20.i686/scripts/module-common.lds --build-id  -o /home/alex.sternberg/projects/forwardfs.git/ffs.ko /home/alex.sternberg/projects/forwardfs.git/ffs.o /home/alex.sternberg/projects/forwardfs.git/ffs.mod.o
